0f96a4c5ad2c8180093df7773d36831c7db40817
Larisa <laurabrovkina@gmail.com>
2025-04-29 15:59:16 +1200

    Init commit

---
+++ MailDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using MimeKit.Text;
+
+var message = new MimeMessage();
+var from = new MailboxAddress("Alice", "alice@example.com");
+message.From.Add(from);
+
+var to = new MailboxAddress("Bob", "bob@examle.com");
+message.To.Add(to);
+message.Subject = "Hi Bob!";
+message.Body = new TextPart(TextFormat.Plain)
+{
+    Text = """
+           Hi Bob,
+           
+           This is that email thing I was telling you about.
+           
+           Pretty neat, huh?
+           
+           -Alice
+           """
+};
+
+using var smtp = new SmtpClient();
+await smtp.ConnectAsync("localhost", 1025);
+await smtp.SendAsync(message);
+await smtp.DisconnectAsync(true);
+
+Console.WriteLine($"Mail sent to {to.Address}");
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # MailDemo
+## Mailpit and Docker
+More info on [Mailpit](https://mailpit.axllent.org/docs/install/docker/)
+* Run docker
+
+## Testing using Telnet
+* Switch on Telnep on Windows (Control Panel -> Programs and Features)
+* Run commands PowerShell:
+```
+telnet localhost 1025
+```
+
+![pic](./img/telnet_message.jpg)
+
+**Note**: There is an issue on this picture, it is supposed to be an empty line for headers before hello, otherwise sending an email command fails
+
+## Received email
+Go to http://localhost:8025/ to see emails in an inbox:
+![pic](./img/mailpit_inbox.jpg)
+* that we sent using telnet commands
+* our console app sent it with the first run
